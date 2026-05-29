925b905e22af3b717e9daafa0eb1e2a6b9962d57
glikesss~~~ <97441336+glikesss@users.noreply.github.com>
2026-05-30 00:58:09 +0700

    Refactor binary tree implementation with new methods

--- bin_tree.py
+++ bin_tree.py
@@ -1,240 +1,272 @@
-
-'''
-
-    func() -> None returns 0 if it completed succesfully or if input value is tecnically correct but meaningless it returns 1
-'''
-
-class Node:
-    def __init__(self, value=None, parent=None, key=None, left=None, right=None):
-        self.left   = left
-        self.right  = right
-        self.key    = key
-        self.value  = value
-        self.parent = parent
-
-
 class Tree:
     def __init__(self, cmp_func):
+        self.cmp_func = cmp_func
         self.root_node = None
-        self.cmp_func  = cmp_func
 
 
-def create(cmp_func) -> Tree:
-    if not callable(cmp_func):
-        return None
+class Node:
+    def __init__(self, value, key):
+        self.left = None
+        self.right = None
+        self.key = key
+        self.value = value
+        self.height = 1
 
-    return Tree(cmp_func)
 
-
-def clear(tree) -> None:
-    ### 1-st variant (delete only brances, and root node)
-    def clear_delete_branches(node):
-        node.left  = None
-        node.right = None
-
-    if not isinstance(tree, Tree) and callable(func):
-        return None
-    temp_list = [True]                                            # List that stores path value (position and depth(by looking at its size-1))
-    if tree.root_node != None:                                    # The last element is the direction of 'view' (future planned step)
-        temp_node = tree.root_node
+def create(cmp_func):
+    if callable(cmp_func):
+        return Tree(cmp_func)
     else:
         return None
 
-    while True:                         
-        if temp_list[-1]:                                         # Check if last step was left
-            if temp_node.left != None:                            # 
-                temp_node = temp_node.left                        # Go left
-                temp_list.append(True)                            # Add last step at the list end (there it's left)
-                continue
-            else:
-                temp_list[-1] = False                             # Set last step to right
-                continue
 
-        else:                                                     
-            if temp_node.right != None:                           # Check if it can go further
-                temp_node = temp_node.right       
-                temp_list.append(True)
-            else:
-                while not temp_list[-1] and len(temp_list) > 1:  # Go upper and execute the function (func) until 'view' isn't 'to the left'
-                    clear_delete_branches(temp_node)
-                    temp_node = temp_node.parent
-                    temp_list.pop()
-                if temp_list[-1]:                                 # Check if it can go on the right      
-                    temp_list[-1] = False
-                else:                                             # If current node is the root node
-                    clear_delete_branches(temp_node)
-                    break
+def insert(tree, value, key):
+    if not isinstance(tree, Tree): return None
+    if tree.root_node == None:
+        tree.root_node = Node(value, key)
+        return None
+
+    def insert_node(node, value, key, cmp_func):
+        if node == None:
+            return Node(value, key), None
+        direction = cmp_func(key, node.key)
+        r = None
+        if direction == -1:
+            t = insert_node(node.left, value, key, cmp_func)
+            node.left, r = t[0], t[1]
+        elif direction == 1:
+            t = insert_node(node.right, value, key, cmp_func)
+            node.right, r = t[0], t[1]
+        
+        elif direction == 0:
+            r = node.value
+            node.value = value
+            node.key   = key
+            return node, r
+
+        # Balansing
+
+        node.height = 1 + max(get_height(node.left), get_height(node.right))
+        balance = get_balance(node)
+
+        if tree.root_node == node:
+            if   balance > 1: tree.root_node = node.left
+            elif balance < -1: tree.root_node = node.right
+
+        # Left Left
+        if balance > 1 and get_balance(node.left) >= 0:
+            return right_rotate(node), r
+
+        # Left Right
+        if balance > 1 and get_balance(node.left) < 0:
+            node.left = left_rotate(node.left)
+            return right_rotate(node), r
+
+        # Right Right
+        if balance < -1 and get_balance(node.right) <= 0:
+            return left_rotate(node), r
+
+        # Right Left
+        if balance < -1 and get_balance(node.right) > 0:
+            node.right = right_rotate(node.right)
+            return left_rotate(node), r
+                    
+        return node, r
+
+
+
+
+    return insert_node(tree.root_node, value, key, tree.cmp_func)[1]
+
+
+                                      
+
+
+def right_rotate(node):
+    temp_node1 = node.left
+    temp_node2 = temp_node1.right
+
+    node.left = temp_node2
+    temp_node1.right = node
+    node.height = 1 + max(get_height(node.left), get_height(node.right))
+
+    temp_node1.height = 1 + max(get_height(temp_node1.left), get_height(temp_node1.right))
+
+    return temp_node1
+
+def left_rotate(node):
+    temp_node1 = node.right
+    temp_node2 = temp_node1.left
+
+    node.right = temp_node2
+    temp_node1.left = node
+
+    node.height = 1 + max(get_height(node.left),
+                          get_height(node.right))
+
+    temp_node1.height = 1 + max(get_height(temp_node1.left),
+                                get_height(temp_node1.right))
+
+    return temp_node1
+
+
+def get_height(node):
+    if not node:
+        return 0
+    return node.height
+    
+
+def get_balance(node):
+    return get_height(node.left) - get_height(node.right) 
+
+
+def foreach(tree, func):
+    if not isinstance(tree, Tree):
+        return None
+    if tree.root_node is None:
+        return None
+
+    def foreach_node(node, func):
+        if node.left != None:
+            foreach_node(node.left, func)
+        if node.right != None:
+            foreach_node(node.right, func)
+        func(node)
+
+    foreach_node(tree.root_node, func)
+
+
+def delete(tree, key):
+    if not isinstance(tree, Tree):
+        return None
+
+    if tree.root_node is None:
+        return None
+
+    def delete_node(node, key, cmp_func):
+
+
+        if node is None:
+            return node, None
+        r = None
+        direction = cmp_func(key, node.key)
+
+        if direction == -1:
+            node.left,  r = delete_node(node.left, key, cmp_func)
+        elif direction == 1:
+            node.right, r = delete_node(node.right, key, cmp_func)
+        elif direction == 0:
+            if node.left is None:
+                temp = node.right
+                r = node.value
+                node = None
+                return temp, r
+            elif node.right is None:
+                temp = node.left
+                r = node.value
+                node = None
+                return temp, r
+            
+            r = node.value
+            temp = min_value_node(node.right)
+            node.value = temp.value
+            node.right = delete_node(node.right, temp.value, cmp_func)[0]
+        return node, r
+
+    if tree.root_node.key == key:
+        temp_node = Node(None, None)
+        temp_node.left = tree.root_node
+        temp_node.left, r = delete_node(temp_node.left, key, tree.cmp_func) 
+        tree.root_node = temp_node.left
+        return r
+
+    return delete_node(tree.root_node, key, tree.cmp_func)[1]
+
+
+def min_value_node(node):
+    temp_node = node
+    while temp_node.left is not None:
+        temp_node = temp_node.left
+    return temp_node
+
+
+def clear(tree):
+    if not isinstance(tree, Tree): return None
+    if tree.root_node == None:
+        return 0
+    def clear_node(node):
+        if node.left != None:
+            clear_node(node.left)
+        if node.right != None:
+            clear_node(node.right)
+        
+        node.left = None
+        node.right = None
+        node = None
+
+    clear_node(tree.root_node)
     tree.root_node = None
 
-
-def sizeof(tree) -> int:
-    if not isinstance(tree, Tree):
-        return -1
-    size = [0]
-    def count(value, key):
-        size[0] += 1
-
-    forall(tree, count)
-    return size[0]
-
-
-def insert(tree: Tree, value: any, key: any) -> None:
-    if not isinstance(tree, Tree):
-        return None
-
-    node = Node(value=value, key=key)
-    
+def sizeof(tree):
+    if not isinstance(tree, Tree): return -1
     if tree.root_node == None:
-            tree.root_node = node
-            return None
-    temp_node = tree.root_node
+        return 0
+    def size_node(node):
+        size = 1
+        if node.left != None:
+            size += size_node(node.left)
+        if node.right != None:
+            size += size_node(node.right)
+        else:
+            return 1
+        return size
 
+    return size_node(tree.root_node)
+ 
 
-    while True:
-        direction = tree.cmp_func(temp_node.key, key)
-        if direction == -1:                         # go through the tree by checking tree.cmp_func condition
-            if temp_node.left == None or temp_node.left.value == node.value:
-                node.parent = temp_node
-                temp_node.left = node
-                break
-            else:
-                temp_node = temp_node.left
-                continue
-        elif direction == 1: 
-            if temp_node.right == None or temp_node.right.value == node.value:
-                node.parent = temp_node
-                temp_node.right = node
-                break
-            else:
-                temp_node = temp_node.right
-                continue
-        
-        temp_node.value = value
-        temp_node.key   = key
-        return (temp_node.value, temp_node.key)
-
-        
-def delete(tree, key) -> any:
-    if not isinstance(tree, Tree):
-        return None
+def find(tree, key):
+    if not isinstance(tree, Tree): return -1
     if tree.root_node == None:
         return None
+    def find_node(node, key):
+        res = None
+        if node.left != None:
+            res = find_node(node.left, key)
+        if node.right != None and res == None:
+            res = find_node(node.right, key) 
+        if node.key == key:
+            res = node.value 
+        return res
+    return find_node(tree.root_node, key)
 
-    temp_node = tree.root_node
-
-    while key != temp_node.key:
-        direction = tree.cmp_func(temp_node.key, key)
-        if temp_node == None:
-            return None
-        
-        if direction == -1:
-            temp_node = temp_node.left
-        elif direction == 1:
-            temp_node = temp_node.right
-        else:
-            break
-
-    if tree.root_node == temp_node:
-        tree.root_node = None
-        temp_value = temp_node.value
-        temp_key   = temp_node.key
-    else:
-        if temp_node.parent.left == temp_node:
-            temp_node.parent.left = None
-        else:
-            temp_node.parent.right = None
-
-    temp_tree_left_node = Tree(tree.cmp_func)
-    temp_tree_left_node.root_node = temp_node.left 
-    temp_tree_right_node = Tree(tree.cmp_func)
-    temp_tree_right_node.root_node = temp_node.right
-
-    def temp_insert(temp_value, temp_key):
-        insert(tree, temp_value, temp_key)
-    
-    forall(temp_tree_left_node , temp_insert)
-    forall(temp_tree_right_node, temp_insert)
-
-    return (temp_node.value, temp_node.key)
-
-
-def forall(tree, func) -> None:
-    if not isinstance(tree, Tree) and callable(func):
-        return None
-    temp_list = [True]                                            # List that stores path value (position and depth(by looking at its size-1))
-    if tree.root_node != None:                                    # The last element is the direction of 'view' (future planned step)
-        temp_node = tree.root_node
-    else:
-        return None
-
-    while True:                         
-        if temp_list[-1]:                                         # Check if last step was left
-            if temp_node.left != None:                            # 
-                temp_node = temp_node.left                        # Go left
-                temp_list.append(True)                            # Add last step at the list end (there it's left)
-                continue
-            else:
-                temp_list[-1] = False                             # Set last step to right
-                continue
-
-        else:                                                     
-            if temp_node.right != None:                           # Check if it can go further
-                temp_node = temp_node.right       
-                temp_list.append(True)
-            else:
-                while not temp_list[-1] and len(temp_list) > 1:  # Go upper and execute the function (func) until 'view' isn't 'to the left'
-                    if func(temp_node.value, temp_node.key):
-                        return 0
-                    temp_node = temp_node.parent
-                    temp_list.pop()
-                if temp_list[-1]:                                 # Check if it can go on the right      
-                    temp_list[-1] = False
-                else:                                             # If current node is the root node
-                    if func(temp_node.value, temp_node.key):
-                        return 0
-                    return 0
-
-
-###########################
-#######  TEST AREA  ####### 
-###########################
-
-
-def main() -> None:
-    def foo(value, key) -> any:
-        return print(f"value = {value}  key = {key}")
-
-    def cmp(key1, key2) -> bool:
-        if key1 < key2:
-            return -1
+def main():
+    def cmp_func(key1, key2):
+        if key1 < key2: return -1
         return int(key1 > key2)
-    tree = create(cmp)
 
+    a = create(cmp_func)
 
-    a = create(cmp)
-    # insert(tree, value, key)
-    insert(a, 1, 1)
-    insert(a, 2, 2)
-    insert(a, 3, 3)
-    insert(a, 4, 4)
-    insert(a, 5, 5)
-    insert(a, 6, 6)
-    insert(a, 7, 7)
+    for i in range(7):
+        insert(a, i, i)
+    
+    def print_all(node):
+        print(node.value)
 
-    forall(a, foo)
-    print(f"Size = {sizeof(a)}")
-
-    print(f"Deleting (value=3)...")
-    delete (a, 3)
-    forall(a, foo)
-    print("Deleted!")
-    print(f"Size = {sizeof(a)}")
-
-    print(f"Clearing...")
+    foreach(a, print_all)
+    print(f"________________________")
+    print(f"Size {sizeof(a)}")
+    print(f"________________________")
+    print(f"DELETED: {delete(a, 3)}")
+    foreach(a, print_all)
+    print(f"________________________")
+    print(f"Size {sizeof(a)}")
+    print(f"Node founded {find(a, 1)}")
     clear(a)
-    print(f"Size = {sizeof(a)}")
+    print(f"________________________")
+    print(f"Size {sizeof(a)}")
+    print(f"________________________")
 
-    forall(a, foo)
+
 
 if __name__ == "__main__":
     main()
